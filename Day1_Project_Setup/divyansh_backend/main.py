c159b6beebd431f264e02f91dcf7628715637c6e
Devdootji <kdivyansh453@gmail.com>
2026-08-14 20:24:44 +0530

    [Day-1-Task-2] divyansh finished step 2

--- Day1_Project_Setup/divyansh_backend/main.py
+++ Day1_Project_Setup/divyansh_backend/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-# App instance create karna
-app = FastAPI(title="Project Netra Backend", description="Backend APIs for AI Threat Detection")
+app = FastAPI(title="Project Netra Backend")
 
-# Health check endpoint - ye verify karne ke liye ki server zinda hai
+# API Data Structure define karna
+class AlertData(BaseModel):
+    camera: str
+    threat: str
+
 @app.get("/health")
 def health_check():
-    return {
-        "status": "success", 
-        "message": "Netra API is running perfectly."
-    }
+    return {"status": "success", "message": "Netra API is running perfectly."}
+
+# Task 2: Naya POST Endpoint
+@app.post("/api/alert")
+def receive_alert(data: AlertData):
+    # Data ko console mein print karna (Goal ke hisaab se)
+    print(f"🚨 ALERT RECEIVED - Camera: {data.camera} | Threat: {data.threat}")
+    return {"status": "success", "data_received": data}
