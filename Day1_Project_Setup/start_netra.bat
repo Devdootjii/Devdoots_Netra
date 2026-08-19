10e9382a719f87308df88e63e9894cb8c9af5402
Devdootji <kdivyansh453@gmail.com>
2026-08-19 16:12:20 +0530

    fronted update

--- Day1_Project_Setup/start_netra.bat
+++ Day1_Project_Setup/start_netra.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Starting Project Netra...
-start "Backend" cmd /k "cd Day1_Project_Setup\divyansh_backend && python -m uvicorn main:app --reload"
+start "Backend" cmd /k "cd divyansh_backend && uvicorn main:app --reload"
 timeout /t 5
-start "AI Pipeline" cmd /k "cd Day1_Project_Setup\balram_task && python balram_task3_freeze.py"
-start "Ritesh Task" cmd /k "cd Day1_Project_Setup\ritesh_task && python ai_hardware_sync.py"
-start "Frontend" cmd /k "cd Day1_Project_Setup\aryan_task && npm run dev"
+start "AI Pipeline" cmd /k "cd balram_yolo_test && python master_ai_script.py"
+echo Starting Frontend UI...
+start cmd /k "cd aryan_task && npm run dev"
 echo System Launched Successfully.
 pause
